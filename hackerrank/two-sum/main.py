821314f32d8fbb40bf55de740e03e6ba65b97f6e
kotaaaa <kota.k.1132.pda@gmail.com>
2022-09-22 20:54:19 +0900

    two sum

---
+++ hackerrank/two-sum/main.py
@@ -0,0 +1,18 @@
+from bisect import bisect_right
+from copy import copy
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums_orig = copy(nums)
+        nums.sort()
+        for i in range(len(nums)):
+            idx = bisect_left(nums[i + 1 :], target - nums[i])
+            if idx + i + 1 >= len(nums):
+                continue
+            if nums[idx + i + 1] == target - nums[i]:
+                break
+        if nums[i] == nums[idx + i + 1]:
+            return [j for j, x in enumerate(nums_orig) if x == nums[i]]
+        else:
+            return [nums_orig.index(nums[i]), nums_orig.index(nums[idx + i + 1])]
