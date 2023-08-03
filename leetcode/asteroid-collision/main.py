9fdb2f027e03c418e7eca7a2f8e6dcd64cb85996
kotaaaa <kota.k.1132.pda@gmail.com>
2023-08-02 17:12:34 -0700

    fix code

--- leetcode/asteroid-collision/main.py
+++ leetcode/asteroid-collision/main.py
@@ -1,27 +1,21 @@
 from collections import deque
-
-
 class Solution:
     def asteroidCollision(self, asteroids: List[int]) -> List[int]:
         q = deque()
         q.append(asteroids[0])
         for i in range(1, len(asteroids)):
             while len(q) >= 0:
-                if asteroids[i] > 0:
+                if asteroids[i] > 0: 
                     q.append(asteroids[i])
-                    break
                 else:
                     if len(q) == 0:
                         q.append(asteroids[i])
-                        break
                     elif q[-1] < 0:
                         q.append(asteroids[i])
-                        break
                     elif q[-1] == -1 * asteroids[i]:
                         q.pop()
-                        break
-                    elif q[-1] > -1 * asteroids[i]:
-                        break
                     elif q[-1] < -1 * asteroids[i]:
                         q.pop()
+                        continue
+                break
         return list(q)
