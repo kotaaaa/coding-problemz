88d56102c6b564f1430871b2cf4333b302d97f5d
kotaaaa <kota.k.1132.pda@gmail.com>
2023-08-03 16:02:46 -0700

    fix

--- atcoder/90typical/58/Main.py
+++ atcoder/90typical/58/Main.py
@@ -1,20 +1,15 @@
-n,k = map(int,input().split())
-if n ==0:
+n, k = map(int, input().split())
+if n == 0:
     print(0)
     exit()
-# if k == 0:
-    # print()
 
 visited = [-1 for _ in range(10**5)]
-# visited[n] = 1
-# mod = 10**5
 cnt = 0
 nums = [-1 for _ in range(10**5)]
 nums[0] = n
 
 cycle = -1
 while True:
-    # print("cnt>>",cnt,n,visited[n])
     if visited[n] >= 0:
         cycle = cnt - visited[n]
         break
@@ -22,24 +17,11 @@ while True:
     nums[cnt] = n
     x = n
     for i in str(n):
-        # print("x",x,i)
         x += int(i)
     n = x % (10**5)
     cnt += 1
-    # nums[cnt] = n
-    # visited[n] = 1
-
-# print(cnt,cycle)
-# print(nums[:cnt+4])
-# print(nums[(k - (cnt - cycle)+1) % cycle])
-# print(nums[(k - (cnt - cycle)) % cycle])
-# print(nums[k % cycle])
-# print(cnt,cycle,visited[n],"n",n)
 
 if k < visited[n]:
     print(nums[k])
     exit()
-print(nums[(k-visited[n]) % cycle + visited[n]])
-# print(nums[9193])
-# print(nums[9194])
-# print(nums[9195])
+print(nums[(k - visited[n]) % cycle + visited[n]])
